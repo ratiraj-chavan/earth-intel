07fc0771b2cd9ed3d7aec096a65a8c03ab1d7632
“ratiraj <ratirajchavan12@gmail.com>
2026-04-24 00:38:26 +0530

    edit req txt

--- backend/services/soil_service.py
+++ backend/services/soil_service.py
@@ -5,10 +5,9 @@ from utils.preprocess import preprocess_image
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-MODEL_PATH = os.path.join(BASE_DIR, "../models/soil_model.h5")
-LABELS_PATH = os.path.join(BASE_DIR, "../models/labels.json")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_PATH = os.path.join(BASE_DIR, "models", "soil_model.h5")
+LABELS_PATH = os.path.join(BASE_DIR, "models", "labels.json")
 model = None
 
 def get_model():
